479328c78becda38314e327475f785a13703f1ea
kuky <kukyxingshi@163.com>
2020-05-11 16:09:52 +0800

    一些调整

--- comvvmhelper/src/main/java/com/kuky/android/comvvmhelper/utils/ApplicationUtils.kt
+++ comvvmhelper/src/main/java/com/kuky/android/comvvmhelper/utils/ApplicationUtils.kt
@@ -2,6 +2,7 @@
 
 package com.kuky.android.comvvmhelper.utils
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageInfo
@@ -18,83 +19,83 @@ import androidx.annotation.RequiresApi
  * @author kuky.
  * @description
  */
-object ApplicationUtils {
-    fun aboveVersion(buildVersion: Int) = Build.VERSION.SDK_INT > buildVersion
 
-    fun onMinVersion(buildVersion: Int) = Build.VERSION.SDK_INT >= buildVersion
+fun Int.aboveVersion() = Build.VERSION.SDK_INT > this
 
-    fun Context.getAppName(): String = try {
-        val packageInfo: PackageInfo = packageManager.getPackageInfo(packageName, 0)
-        val labelRes = packageInfo.applicationInfo.labelRes
-        resources.getString(labelRes)
-    } catch (e: NameNotFoundException) {
+fun Int.onMinVersion() = Build.VERSION.SDK_INT >= this
+
+fun Context.getAppName(): String = try {
+    val packageInfo: PackageInfo = packageManager.getPackageInfo(packageName, 0)
+    val labelRes = packageInfo.applicationInfo.labelRes
+    resources.getString(labelRes)
+} catch (e: NameNotFoundException) {
+    e.printStackTrace()
+    ""
+}
+
+fun Context.getAppVersionName(): String = try {
+    val packageInfo: PackageInfo = packageManager.getPackageInfo(packageName, 0)
+    packageInfo.versionName
+} catch (e: NameNotFoundException) {
+    e.printStackTrace()
+    ""
+}
+
+@SuppressLint("NewApi")
+fun Context.getAppVersionCode(): Long =
+    if (Build.VERSION_CODES.P.onMinVersion()) getLongAppVersion() else getAppIntVersion().toLong()
+
+private fun Context.getAppIntVersion(): Int = try {
+    packageManager.getPackageInfo(packageName, 0).versionCode
+} catch (e: NameNotFoundException) {
+    e.printStackTrace()
+    0
+}
+
+@RequiresApi(Build.VERSION_CODES.P)
+private fun Context.getLongAppVersion(): Long = try {
+    packageManager.getPackageInfo(packageName, 0).longVersionCode
+} catch (e: NameNotFoundException) {
+    e.printStackTrace()
+    0L
+}
+
+fun Context.starApp(packageName: String, fail: () -> Unit) =
+    try {
+        startActivity(Intent(Intent.ACTION_MAIN).apply {
+            addCategory(Intent.CATEGORY_LAUNCHER)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            component = packageManager.getLaunchIntentForPackage(packageName)?.component
+        })
+    } catch (e: Exception) {
         e.printStackTrace()
-        ""
+        fail()
     }
 
-    fun Context.getAppVersionName(): String = try {
-        val packageInfo: PackageInfo = packageManager.getPackageInfo(packageName, 0)
-        packageInfo.versionName
-    } catch (e: NameNotFoundException) {
-        e.printStackTrace()
-        ""
-    }
+fun Context.getAppIcon(pkgName: String): Bitmap? = packageManager.let { pm ->
+    try {
+        val drawable = pm.getApplicationIcon(pkgName)
 
-    fun Context.getAppVersionCode(): Long =
-        if (onMinVersion(Build.VERSION_CODES.P)) getLongAppVersion() else getAppIntVersion().toLong()
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O)
+            return (drawable as BitmapDrawable).bitmap
+        else
+            when (drawable) {
+                is BitmapDrawable -> drawable.bitmap
 
-    private fun Context.getAppIntVersion(): Int = try {
-        packageManager.getPackageInfo(packageName, 0).versionCode
-    } catch (e: NameNotFoundException) {
-        e.printStackTrace()
-        0
-    }
-
-    @RequiresApi(Build.VERSION_CODES.P)
-    private fun Context.getLongAppVersion(): Long = try {
-        packageManager.getPackageInfo(packageName, 0).longVersionCode
-    } catch (e: NameNotFoundException) {
-        e.printStackTrace()
-        0L
-    }
-
-    fun Context.starApp(packageName: String, fail: () -> Unit) =
-        try {
-            startActivity(Intent(Intent.ACTION_MAIN).apply {
-                addCategory(Intent.CATEGORY_LAUNCHER)
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                component = packageManager.getLaunchIntentForPackage(packageName)?.component
-            })
-        } catch (e: Exception) {
-            e.printStackTrace()
-            fail()
-        }
-
-    fun Context.getAppIcon(pkgName: String): Bitmap? = packageManager.let { pm ->
-        try {
-            val drawable = pm.getApplicationIcon(pkgName)
-
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O)
-                return (drawable as BitmapDrawable).bitmap
-            else
-                when (drawable) {
-                    is BitmapDrawable -> drawable.bitmap
-
-                    is AdaptiveIconDrawable -> {
-                        val layerDrawable = LayerDrawable(arrayOf(drawable.background, drawable.foreground))
-                        val width = layerDrawable.intrinsicWidth
-                        val height = layerDrawable.intrinsicHeight
-                        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-                        val canvas = Canvas(bitmap)
-                        layerDrawable.setBounds(0, 0, canvas.width, canvas.height)
-                        layerDrawable.draw(canvas)
-                        bitmap
-                    }
-                    else -> null
+                is AdaptiveIconDrawable -> {
+                    val layerDrawable = LayerDrawable(arrayOf(drawable.background, drawable.foreground))
+                    val width = layerDrawable.intrinsicWidth
+                    val height = layerDrawable.intrinsicHeight
+                    val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+                    val canvas = Canvas(bitmap)
+                    layerDrawable.setBounds(0, 0, canvas.width, canvas.height)
+                    layerDrawable.draw(canvas)
+                    bitmap
                 }
-        } catch (e: NameNotFoundException) {
-            e.printStackTrace()
-            null
-        }
+                else -> null
+            }
+    } catch (e: NameNotFoundException) {
+        e.printStackTrace()
+        null
     }
 }
--- comvvmhelper/src/main/java/com/kuky/android/comvvmhelper/utils/SharePreferencesUtils.kt
+++ comvvmhelper/src/main/java/com/kuky/android/comvvmhelper/utils/SharePreferencesUtils.kt
@@ -6,45 +6,43 @@ import android.content.Context
  * @author kuky.
  * @description
  */
-object SharePreferencesUtils {
-    private const val SHARED_PREFERENCES_NAME = "com.kkandroid.commvmhelper.share.preference"
+private const val SHARED_PREFERENCES_NAME = "com.kkandroid.commvmhelper.share.preference"
 
-    private fun Context.defaultSharePreferences() =
-        getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
+private fun Context.defaultSharePreferences() =
+    getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
 
-    fun Context.saveString(key: String, value: String) =
-        defaultSharePreferences().edit().putString(key, value).apply()
+fun Context.saveString(key: String, value: String) =
+    defaultSharePreferences().edit().putString(key, value).apply()
 
-    fun Context.getString(key: String, default: String = ""): String =
-        defaultSharePreferences().getString(key, default) ?: default
+fun Context.getString(key: String, default: String = ""): String =
+    defaultSharePreferences().getString(key, default) ?: default
 
-    fun Context.saveInteger(key: String, value: Int) =
-        defaultSharePreferences().edit().putInt(key, value).apply()
+fun Context.saveInteger(key: String, value: Int) =
+    defaultSharePreferences().edit().putInt(key, value).apply()
 
-    fun Context.getInteger(key: String, default: Int = 0): Int =
-        defaultSharePreferences().getInt(key, default)
+fun Context.getInteger(key: String, default: Int = 0): Int =
+    defaultSharePreferences().getInt(key, default)
 
-    fun Context.saveLong(key: String, value: Long) =
-        defaultSharePreferences().edit().putLong(key, value).apply()
+fun Context.saveLong(key: String, value: Long) =
+    defaultSharePreferences().edit().putLong(key, value).apply()
 
-    fun Context.getLong(key: String, default: Long = 0L): Long =
-        defaultSharePreferences().getLong(key, default)
+fun Context.getLong(key: String, default: Long = 0L): Long =
+    defaultSharePreferences().getLong(key, default)
 
-    fun Context.saveFloat(key: String, value: Float) =
-        defaultSharePreferences().edit().putFloat(key, value).apply()
+fun Context.saveFloat(key: String, value: Float) =
+    defaultSharePreferences().edit().putFloat(key, value).apply()
 
-    fun Context.getFloat(key: String, default: Float = 0F): Float =
-        defaultSharePreferences().getFloat(key, default)
+fun Context.getFloat(key: String, default: Float = 0F): Float =
+    defaultSharePreferences().getFloat(key, default)
 
-    fun Context.saveBoolean(key: String, value: Boolean) =
-        defaultSharePreferences().edit().putBoolean(key, value).apply()
+fun Context.saveBoolean(key: String, value: Boolean) =
+    defaultSharePreferences().edit().putBoolean(key, value).apply()
 
-    fun Context.getBoolean(key: String, default: Boolean = false): Boolean =
-        defaultSharePreferences().getBoolean(key, default)
+fun Context.getBoolean(key: String, default: Boolean = false): Boolean =
+    defaultSharePreferences().getBoolean(key, default)
 
-    fun Context.saveStringSet(key: String, value: MutableSet<String>?) =
-        defaultSharePreferences().edit().putStringSet(key, value).apply()
+fun Context.saveStringSet(key: String, value: MutableSet<String>?) =
+    defaultSharePreferences().edit().putStringSet(key, value).apply()
 
-    fun Context.getStringSet(key: String): MutableSet<String> =
-        defaultSharePreferences().getStringSet(key, mutableSetOf()) ?: mutableSetOf()
-}
+fun Context.getStringSet(key: String): MutableSet<String> =
+    defaultSharePreferences().getStringSet(key, mutableSetOf()) ?: mutableSetOf()
