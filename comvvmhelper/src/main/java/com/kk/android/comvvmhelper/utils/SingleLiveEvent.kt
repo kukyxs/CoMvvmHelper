4c4c476ac53118345d7a5cbe5764bf69259e5b7f
kuky <kukyxingshi@163.com>
2020-09-16 11:47:32 +0800

    SingleLiveEvent and UnPeekLiveData

---
+++ comvvmhelper/src/main/java/com/kk/android/comvvmhelper/utils/SingleLiveEvent.kt
@@ -0,0 +1,40 @@
+package com.kk.android.comvvmhelper.utils
+
+import android.util.Log
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
+
+/**
+ * @author kuky.
+ * @description single observer for LiveData or use [UnPeekLiveData]
+ */
+class SingleLiveEvent<T> : LiveData<T>() {
+    private val mPending = AtomicBoolean(false)
+
+    @MainThread
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        if (hasActiveObservers()) {
+            Log.w(SingleLiveEvent::class.java.simpleName, "Multiple observers registered but only one will be notified of changes.")
+        }
+
+        super.observe(owner, {
+            if (mPending.compareAndSet(true, false)) {
+                observer.onChanged(it)
+            }
+        })
+    }
+
+    @MainThread
+    override fun setValue(value: T?) {
+        mPending.set(true)
+        super.setValue(value)
+    }
+
+    @MainThread
+    fun call() {
+        value = null
+    }
+}
---
+++ comvvmhelper/src/main/java/com/kk/android/comvvmhelper/utils/UnPeekLiveData.kt
@@ -0,0 +1,88 @@
+package com.kk.android.comvvmhelper.utils
+
+import android.util.Log
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import com.kk.android.comvvmhelper.extension.otherwise
+import com.kk.android.comvvmhelper.extension.yes
+import java.util.*
+import kotlin.concurrent.timerTask
+
+/**
+ * @author kuky.
+ * @description 防止 LiveData 数据倒灌
+ * @reference https://github.com/KunMinX/UnPeek-LiveData
+ */
+open class ProtectedUnPeekLiveData<T> : LiveData<T>() {
+    private var isCleaning = false
+    private var hasHandled = true
+    private var isDelaying = false
+
+    private val mTimer = Timer()
+    private var mTimerTask: TimerTask? = null
+
+    protected var delayToCleanEvent = 1000L
+    protected var isAllowNullValue = false
+    protected var isAllowToClean = true
+
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        super.observe(owner, {
+            isCleaning.yes {
+                hasHandled = true; isDelaying = false; isCleaning = false
+            }.otherwise {
+                hasHandled.yes { observer.onChanged(it) }
+                    .otherwise { hasHandled = true; isDelaying = true; observer.onChanged(it) }
+            }
+        })
+    }
+
+    override fun observeForever(observer: Observer<in T>) {
+        throw IllegalArgumentException("Do not use observeForever for communication between pages to avoid lifecycle security issues")
+    }
+
+    override fun setValue(value: T) {
+        if (!isCleaning && (!isAllowNullValue && value == null)) {
+            return
+        }
+
+        hasHandled = false
+        isDelaying = false
+        super.setValue(value)
+
+        mTimerTask?.let { it.cancel(); mTimer.purge() }
+
+        value?.let { _ ->
+            val task = timerTask { clear() }.also { mTimerTask = it }
+            mTimer.schedule(task, delayToCleanEvent)
+        }
+    }
+
+    private fun clear() {
+        isAllowToClean.yes { isCleaning = true; super.postValue(null) }
+            .otherwise { hasHandled = true; isDelaying = false }
+    }
+}
+
+class UnPeekLiveData<T> : ProtectedUnPeekLiveData<T>() {
+    override fun setValue(value: T) {
+        super.setValue(value)
+        Log.i(UnPeekLiveData::class.java.simpleName, "setValue")
+    }
+
+    override fun postValue(value: T) {
+        super.postValue(value)
+        Log.i(UnPeekLiveData::class.java.simpleName, "postValue")
+    }
+
+    data class UnPeekLiveDataBuilder<T>(
+        val eventSurvivalTime: Long = 1000,
+        val allowNullValue: Boolean, val allowToClean: Boolean = true
+    ) {
+        fun create(): UnPeekLiveData<T> = UnPeekLiveData<T>().apply {
+            delayToCleanEvent = eventSurvivalTime
+            isAllowNullValue = allowNullValue
+            isAllowToClean = allowToClean
+        }
+    }
+}
