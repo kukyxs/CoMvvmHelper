eca084baba0685da6723d5d6f78848cb2b122059
kuky <kukyxingshi@163.com>
2021-04-27 11:35:18 +0800

    mmkv support

--- app/src/main/java/com/kuky/comvvmhelper/viewmodel/PagingViewModel.kt
+++ app/src/main/java/com/kuky/comvvmhelper/viewmodel/PagingViewModel.kt
@@ -1,10 +1,10 @@
 package com.kuky.comvvmhelper.viewmodel
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
-import com.kk.android.comvvmhelper.ui.CovViewModel
 import com.kuky.comvvmhelper.pagingsource.ArticlePagingSource
 import com.kuky.comvvmhelper.repository.ArticleRepository
 
@@ -13,7 +13,7 @@ import com.kuky.comvvmhelper.repository.ArticleRepository
  * @description
  */
 
-class PagingViewModel(private val repository: ArticleRepository) : CovViewModel() {
+class PagingViewModel(private val repository: ArticleRepository) : ViewModel() {
 
     fun articleList() = Pager(PagingConfig(20)) {
         ArticlePagingSource(repository)
